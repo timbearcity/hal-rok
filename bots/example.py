570a4bbd2dd634770978d950e26625b4d5465512
Tim Tissari Bjornstedt <timtissaribjornstedt@gmail.com>
2020-10-03 15:10:59 +0100

    Add example bot functionality

---
+++ bots/example.py
@@ -0,0 +1,20 @@
+import discord
+
+from secrets import TOKEN
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
